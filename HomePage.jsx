c5465dd11f3bcdfdd900f8d2dfe7724fa925ad7d
paras pasbola <69096712+paraspasbola@users.noreply.github.com>
2024-07-16 16:35:28 +0530

    added src/components/homepage/

---
+++ HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = ({setLoginUser}) => {
+  return (
+    <div className='homepage'>
+      <h1>homepage</h1>
+      <div className="button" onClick={() => setLoginUser({})}>Logout</div>
+      
+    </div>
+  )
+}
+
+export default HomePage;
